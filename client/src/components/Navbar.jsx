66ae294b755b2cd16f3362ff5891030b03255057
riku.toivanen123@gmail.com <riku.toivanen123@gmail.com>
2025-02-06 00:55:13 +0200

    journalbox

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -15,9 +15,14 @@ const Navbar = () => {
     return(
         <div className="p-0 m-0"> 
             <nav className={`flex justify-between items-center sticky top-0 z-10 py-4 pl-4 ${theme.backgroundViolet}`}>
-                <div>
-                    <h1 className="text-3xl font-bold text-white"><Link to="/">Daily Balance</Link></h1>
-                </div>
+            <div className="flex gap-6">
+                <h1 className="text-3xl font-bold text-white">
+                    <Link to="/">Daily Balance</Link>
+                </h1>
+                <h1 className="text-3xl font-bold text-white">
+                    <Link to="/journal">Journal</Link>
+                </h1>
+            </div>
                 <ul className="flex font-semibold items-center space-x-4 mr-4">
                     {isLoggedIn ? (
                         <>
